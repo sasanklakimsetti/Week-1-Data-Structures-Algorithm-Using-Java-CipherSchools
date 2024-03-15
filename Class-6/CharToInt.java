5e52cd848f9b08692b6b978702128340fda4acd1
sasanklakimsetti <saisasank.lakimsetti@gmail.com>
2024-03-15 17:12:45 +0530

    Class 6

---
+++ Class-6/CharToInt.java
@@ -0,0 +1,9 @@
+public class CharToInt {
+    public static void main(String[] args){
+        char c='a';
+        System.out.println((int)c);   //getting the ASCII values of char c
+        char c2='b';
+        int e=c2-'a';  //subtracting ASCII value of c2 from ASCII value of 'a'
+        System.out.println(e);
+    }
+}
